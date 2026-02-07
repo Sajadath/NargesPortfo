2ad6fa9068480244e40ab98c2f369fb28a7ba446
sajad <sajadath1@gmail.com>
2026-02-07 18:20:59 +0330

    add full bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,24 @@
 import HeroSection from "@/components/heroSection/HeroSection";
+import Image from "next/image";
+import mainBg from "../../public/main/highMainBackground.png";
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-[url('/main/highMainBasdckground.png')] bg-cover bg-center relative ">
+      <Image
+        fill
+        placeholder="blur"
+        quality={100}
+        className="object-cover -z-1"
+        src={mainBg}
+        alt="mainBG"
+      />
+
+      <HeroSection />
+      <HeroSection />
+      <HeroSection />
+      <HeroSection />
+      <HeroSection />
       <HeroSection />
       <HeroSection />
     </main>
--- src/components/heroSection/HeroSection.tsx
+++ src/components/heroSection/HeroSection.tsx
@@ -2,15 +2,31 @@ import DvhBoxContainer from "../ui/DvhBoxContainer";
 
 function HeroSection() {
   return (
-    <DvhBoxContainer withBackground>
+    <DvhBoxContainer>
       <div className="w-full flex items-center h-full text-lightGray relative ">
         <div>
           <h3>Updated</h3>
           <div className="flex ">
             <div className="flex flex-col">
-              <h1 className="font-impact text-5xl sm:text-7xl md:text-9xl lg:text-[10rem] xl:text-[15rem] py-0 ">
-                PORTFOLIO
-              </h1>
+              <div className="relative">
+                <h1
+                  className="
+                  absolute top-full left-0
+                  font-impact
+                  text-5xl sm:text-7xl md:text-9xl lg:text-[10rem] xl:text-[15rem]
+                  opacity-20
+                  blur-sm
+                  scale-y-[-1]
+                  translate-y-4
+                  skew-x-6
+                  pointer-events-none "
+                >
+                  PORTFOLIO
+                </h1>
+                <h1 className="font-impact text-5xl sm:text-7xl md:text-9xl lg:text-[10rem] xl:text-[15rem] py-0 ">
+                  PORTFOLIO
+                </h1>
+              </div>
               <div className="flex items-center justify-center text-xs  ">
                 <h3 className="pr-2">Graphic Design</h3>
                 <span className=" h-px grow shrink-0 bg-lightGray/70"></span>
